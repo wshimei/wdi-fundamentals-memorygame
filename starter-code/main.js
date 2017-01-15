06bafc9bff40229452f8a1bca4a108add5812a11
Shimei <wshimei@gmail.com>
2017-01-15 17:36:48 +0800

    edited javascript to create cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,10 +4,24 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "queen";
 
-if (cardOne === cardTwo) {
+/* if (cardOne === cardTwo) {
 	alert("You found a match!")
 } else if (cardThree === cardFour) {
 	alert("You found a match!")
 } else {
 	alert ("Sorry, try again.")
+}*/ 
+
+var gameBoard = document.getElementById('game-board');
+
+for (var i = 0; i <= 4; i++) {
+	var cards = document.createElement('div').innerHTML;
+	cards.className = 'card';
+	document.getElementById('game-board').appendChild(cards);
 }
+
+var createCards = function() {
+
+}
+
+createCards();
