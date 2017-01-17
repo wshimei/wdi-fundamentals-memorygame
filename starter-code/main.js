32389109faafb345dbde721a20e2132e68d153f2
Shimei <wshimei@gmail.com>
2017-01-17 21:01:35 +0800

    completed unit 11 ass

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,37 +1,32 @@
-console.log("JS file is connected to HTML! Woo!")
-
 var cards = ["queen", "queen", "king", "king"]
-
 var cardsInPlay = [];
 
-var createCards = function() {
-  var gameBoard = document.getElementById('game-board');
+// function createCards() {
+//   var gameBoard = document.getElementById('game-board');
 
-  for (var i = 0; i < 4; i++) {
-    var cards = document.createElement('div');
-    cards.className = 'card';
-    gameBoard.appendChild(cards);
-  }
+//   for (var i = 0; i < 4; i++) {
+//     var cards = document.createElement('div');
+//     cards.className = 'card';
+//     gameBoard.appendChild(cards);
+//   }
 
-}
+// }
+// createCards();
 
-createCards();
-
+var board = document.getElementById('game-board');
 function createBoard() {
-  for (var i = 0; i < cards.length; i++);
-    var cardsElements = document.createElement('div');
-    cardsElements.className = 'card';
-    cardsElements.setAttribute('data-card',cards[i]);
-    cardsElements.addEventListener('click',isTwoCards);
+  for (var i = 0; i < cards.length; i++) {
+    var cardElements = document.createElement('div');
+    cardElements.className = 'card';
+    cardElements.setAttribute('data-card',cards[i]);
+    cardElements.addEventListener('click',isTwoCards);
     board.appendChild(cardElements);
     board.appendChild(cardElements);
-
+  }
 }
 
-createBoard();
-
-function isMatch() {
-  if (card[0] === card[1]) {
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
     alert("You found a match!")
   } else {
     alert ("Sorry, try again.")
@@ -39,21 +34,17 @@ function isMatch() {
 }
 
 function isTwoCards() {
-  cardsInPlay.push(this.getAttribute('date-card'));
-  console.log(this.getAttribute('date-card'));
+  cardsInPlay.push(this.getAttribute('data-card'));
+  console.log(this.getAttribute('data-card'));
   if (this.getAttribute('data-card') === 'king') {
-    this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>"; // king
+    this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>";
   } else {
-    this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>"; //queen
+    this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>";
   }
-  // if you have two cards in play check for a match
   if (cardsInPlay.length === 2) {
-    // pass the cardsInPlay as an argument to isMatch function
     isMatch(cardsInPlay);
-    // clear cards in play array for next try
     cardsInPlay = [];
+  }
 }
-}
 
-
-
+createBoard();
